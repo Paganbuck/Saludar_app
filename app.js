71a84ef06e092c6acc90677f8f08e600b651cd37
Juan Diego Bedoya G <jdbedoya928@gmail.com>
2022-10-14 19:51:44 -0500

    Agregando algo de CSS

---
+++ app.js
@@ -0,0 +1,14 @@
+
+let nombre = 'Grover'; 
+console.log(nombre); 
+console.log(typeof(nombre));
+
+
+
+function miFuncion(){
+    let name = document.getElementById('nombre').value;
+    document.write('Hola '+name+ " ¿Cómo estás?" );
+
+
+}
+
